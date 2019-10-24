85aaa99ab6c7fcf567ad61b02453111d23a541f5
Sabeel Ahmad <sabeelbasharat@gmail.com>
2019-10-24 19:02:16 +0530

    add cycle expense calc

--- src/ExpenseCycle.js
+++ src/ExpenseCycle.js
@@ -1,5 +1,27 @@
 import React, { useState } from "react";
 import InputField from "./InputField";
+import Alert from "./Alert";
+import sumExpenses from "./utility/sumExpenses";
+
+const checkInRange = (date, from, to) => {
+  const check = new Date(date);
+  const nfrom = new Date(from);
+  const nto = new Date(to);
+
+  return check >= nfrom && check <= nto;
+};
+
+const computeData = (start, end) => {
+  // Filter expenses based on range
+  const expenses = JSON.parse(localStorage.getItem("Expenses"));
+  if (expenses && expenses.length) {
+    const filtered = expenses.filter(expense => {
+      return checkInRange(expense.date, start, end);
+    });
+    return sumExpenses(filtered);
+  }
+  return 0;
+};
 
 /* The expense cycle component maintains two pieces of information in its state:
  *  a) The start date of the income cycle
@@ -8,15 +30,15 @@ import InputField from "./InputField";
 const ExpenseCycle = ({ today }) => {
   const startHook = useState(today);
   const endHook = useState(today);
-
+  const [isCalculated, setIsCalculated] = useState(0);
   return (
     <div style={{ marginBottom: "50px" }} className="container text-center">
       <form
         onSubmit={e => {
           e.preventDefault();
-          // Add billing cycle details to localStorage
-          localStorage.setItem("cycleStart", startHook[0]);
-          localStorage.setItem("cycleEnd", endHook[0]);
+          const start = new Date(startHook[0]);
+          const end = new Date(endHook[0]);
+          setIsCalculated(computeData(start, end));
         }}
       >
         <h4>Set Expense Cycle</h4>
@@ -39,6 +61,12 @@ const ExpenseCycle = ({ today }) => {
           the selected period
         </small>
       </form>
+      {isCalculated ? (
+        <div style={{ marginTop: "35px" }}>
+          <p>{`Expense data from ${startHook[0]} to ${endHook[0]}`}</p>
+          <Alert val={isCalculated} />
+        </div>
+      ) : null}
     </div>
   );
 };
